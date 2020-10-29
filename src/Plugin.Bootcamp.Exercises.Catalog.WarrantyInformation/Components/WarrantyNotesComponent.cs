20b52a39116bf38bef3f4cb8eea359074ceb1440
Developer Name <Developer Email Address>
2020-10-29 07:16:11 -0500

    Lab 2 warranty notes component

--- src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Components/WarrantyNotesComponent.cs
+++ src/Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation/Components/WarrantyNotesComponent.cs
@@ -5,5 +5,9 @@ namespace Plugin.Bootcamp.Exercises.Catalog.WarrantyInformation.Components
     public class WarrantyNotesComponent : Component
     {
         /* STUDENT: Add properties as specified in the requirements */
+        public string WarrantyInformation { get; set; } = string.Empty;
+
+        public int NumberOfYears { get; set; } = 1;
     }
+   
 }
